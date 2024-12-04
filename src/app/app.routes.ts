6872c2be0d15a045068e1edb4bc463019ff33de6
ldex <laurent.duveau+git@gmail.com>
2024-12-04 07:59:07 -0500

    Lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './shared/home.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
 import { ContactComponent } from './shared/contact.component';
 import { AdminComponent } from './shared/admin.component';
 import { ErrorComponent } from './shared/error.component';
@@ -10,7 +9,10 @@ export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent, title: 'Home Page' },
     { path: 'products', children: [
-        { path: '', component: ProductListComponent, title: 'Products Page' },
+        { path: '', loadComponent: () =>
+                    import('./products/product-list/product-list.component')
+                    .then(r => r.ProductListComponent),
+            title: 'Products Page' },
         { path :':id', component: ProductDetailComponent }
     ]},
     { path: 'contact', component: ContactComponent, title: 'Contact' },
--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, Input } from '@angular/core';
 import { Product } from '../../models/product.interface';
-import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { AsyncPipe, CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [CurrencyPipe, DatePipe, UpperCasePipe],
+  imports: [CurrencyPipe, DatePipe, UpperCasePipe, AsyncPipe],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
@@ -16,15 +18,21 @@ export class ProductDetailComponent {
   private productService = inject(ProductService)
 
   product: Product;
+  product$: Observable<Product>
 
   constructor() {
     let id = this.activatedRoute.snapshot.params.id
 
-    this.productService
-            .getProductById(id)
-            .subscribe(
-              result => this.product = result
-            )
+    this.product$ = this.productService.getProductById(id)
+
+    // this.productService
+    //         .getProductById(id)
+    //         .pipe(
+    //           takeUntilDestroyed()
+    //         )
+    //         .subscribe(
+    //           result => this.product = result
+    //         )
   }
 
 }
